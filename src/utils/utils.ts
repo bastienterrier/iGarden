998a9aa70762e0044f6e417a970138e8803d4561
bterrier <bastien.terrier@etu.utc.fr>
2019-09-15 17:45:33 +0200

    refactor: remove unused methods in Utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -83,34 +83,6 @@ export class Utils {
     return this.users.map(u => u.color);
   }
 
-  public static copyEggsCollect(source: EggsCollect, copy: EggsCollect) {
-    source.picker = copy.picker;
-    source.number = copy.number;
-    source.date = new Date(copy.date);
-  }
-
-  public static copyObject(src: any, copy: any) {
-    Object.keys(copy).forEach(e => {
-      if (e === 'date') {
-        src[e] = new Date(copy[e]);
-      } else {
-        src[e] = copy[e];
-      }
-    });
-  }
-
-  public static copyArray(src: any[], copy: any[]) {
-    if (src.length > 0) {
-      src = new Array();
-    }
-    copy.forEach(e => {
-      src.push(e);
-    });
-    src.forEach((e, i) => {
-      this.copyObject(e, copy[i]);
-    });
-  }
-
   public static translateTodoState(state: string): ToDoState {
     switch (state) {
       case 'WIP':
